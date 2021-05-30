b2b096f66963293276051d03b2afbc311135df14
vmmadathil <madathilvisakh@gmail.com>
2021-05-30 18:16:00 -0500

    fixing main function

--- src/main.py
+++ src/main.py
@@ -72,4 +72,5 @@ def create(username):
 
     return 'Made Playlist'
 
-app.run(host='127.0.0.1', port=8080, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
